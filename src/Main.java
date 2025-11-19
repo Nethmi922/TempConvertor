d45143783eb5737b41155a45f2048bc804d5920d
nethmi <nnavodya922@gmail.com>
2025-11-19 21:07:14 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        double temperature;
+        double newTemperature;
+        String unit;
+
+        System.out.println();
+        System.out.println("------**Temperature Conversion Program**------");
+        System.out.println();
+
+        System.out.print("Enter the temperature: ");
+        temperature = scanner.nextDouble();
+
+        System.out.print("Enter the unit(F/C): ");
+        unit = scanner.next().toUpperCase();
+
+        newTemperature = (unit.equals("C")) ? (temperature*5/9)+32 : (temperature-32)*5/9;
+
+        System.out.printf("%.2f°%s in °%s is: %.2f\n",temperature,unit,(unit.equals("C")) ? "F":"C",newTemperature);
+
+
+        scanner.close();
+
     }
 }
